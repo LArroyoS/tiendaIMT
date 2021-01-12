8c330174fde3783aae949856213a7f80ec4bcb44
Leo <larroyos1@toluca.tecnm.mx>
2021-01-12 16:45:59 -0600

    productos 1

--- Vistas/Modulos/productos.php
+++ Vistas/Modulos/productos.php
@@ -1,1 +1,38 @@
-<h1>PRODUCTOS</h1>
+<?php 
+
+    $item = "ruta";
+    $valor = $rutas[0];
+
+    $categorias = ControladorProductos::ctrMostrarCategorias($item,$valor);
+
+    //var_dump($categorias['id']);
+    $itemP = '';
+    $valorP = '';
+
+    if(!$categorias){
+
+        $subCategorias = ControladorProductos::ctrMostrarSubCategorias($item,$valor);
+        //var_dump($subCategorias[0]['id']);
+
+        $itemP = 'id_subcategoria';
+        $valorP = $subCategorias[0]['id'];
+
+    }else{
+
+        $itemP = 'id_categoria';
+        $valorP = $categorias['id'];
+
+    }
+
+    $ordenar = 'id';
+    $productos = ControladorProductos::ctrMostrarProductos($ordenar,$itemP,$valorP);
+
+    var_dump($productos);
+
+    if(!$productos){
+
+        echo 'Aun no hay productos en esta sección';
+
+    }
+
+?>
