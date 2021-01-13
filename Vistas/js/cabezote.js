6dab2908137951288b99484238540101f71d9457
Leo <larroyos1@toluca.tecnm.mx>
2021-01-13 16:38:40 -0600

    mejora de menu categorias

--- Vistas/js/cabezote.js
+++ Vistas/js/cabezote.js
@@ -16,3 +16,21 @@ $('#btnCategorias').on('click',function(e){
     }
 
 });
+
+$(window).resize(function(){
+
+    var categorias = $("#categorias").css('display');
+    console.log(categorias);
+    if( (categorias=='block') && 
+        (window.matchMedia("(max-width:576px)").matches)){
+
+        $('#btnCategorias').after($('#categorias'));
+
+    }
+    else if((categorias=='block')){
+
+        $('#encabezado').after($('#categorias'));
+
+    }
+
+});
